e3f4ba66dd334dfe441894b797b87466729aca95
dylanharper <harper.dylan@gmail.com>
2019-11-17 11:26:38 -0800

    adding collector for twitter timelines

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# vaqmr
+# Vaqmr
+
+## Deploy a collector
+
+```bash
+gcloud functions deploy twitter_faves --runtime python37 --trigger-topic twitter-faves
+```
+
+## Adding a new collector
+
+Add new function in main.py.
+
+Add new config in config.yml
+
+Create a new Pub/Sub:
+
+```bash
+gcloud pubsub topics create twitter-faves
+gcloud pubsub subscriptions create twitter-faves-sub --topic twitter-faves
+```
+
+Create a schedule:
+
+- <https://console.cloud.google.com/cloudscheduler?project=dotufp>
+
+- Create Job -> Name = twitter-faves -> Target = Pub/Sub -> Topic = twitter-faves
+
+Deploy collector
+
+Test function:
+
+- <https://console.cloud.google.com/cloudscheduler?project=dotufp>
+
+- Find the schedule and click Run Now.
+
+- Check function logs.
+
+- Check output file.
--- main.py
+++ main.py
@@ -21,7 +21,33 @@ def twitter_faves(event, context):
 
     for account in config['work_list']:
         url = 'https://api.twitter.com/1.1/favorites/list.json'
-        params = {'count': 200, 'screen_name': account}
+        params = {'count': 100, 'screen_name': account}
+        headers = {'Authorization': secrets['twitter_bearer']}
+
+        faves = requests.get(url, params=params, headers=headers)
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        blob_name = f'{config["output_prefix"]}{account}/{timestamp}.raw'
+
+        vaqmr.upload_data(config['output_bucket'], blob_name, faves.text)
+
+def twitter_timeline(event, context):
+    """Collect recent tweets/retweets for twitter accounts defined in project config. Triggered by Pub/Sub.
+
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+
+    """
+    config = vaqmr.get_config('twitter_timeline')
+    secrets = vaqmr.get_secrets()
+
+    for account in config['work_list']:
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        params = {'count': 50, 'screen_name': account, 'include_rts': True}
         headers = {'Authorization': secrets['twitter_bearer']}
 
         faves = requests.get(url, params=params, headers=headers)
