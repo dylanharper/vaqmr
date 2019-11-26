3f5c35f552fc99a213a6de6f851c33f5bde71ab6
dylanharper <harper.dylan@gmail.com>
2019-11-25 22:06:11 -0800

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 """Google Functions that collect data for DOTUFP, and are triggered by Pub/Sub."""
 
 import vaqmr
-import requests
-from requests_oauthlib import OAuth1
-from datetime import datetime
 import base64
 import json
 
@@ -24,81 +21,12 @@ def vaqmr(event, context):
     # work_list = event_data.get('work_list')
 
     if collector == 'twitter_faves':
-        twitter_faves()
+        vaqmr.twitter_faves()
     elif collector == 'twitter_timeline':
-        twitter_timeline()
+        vaqmr.twitter_timeline()
     elif collector == 'twitter_home_timeline':
-        twitter_home_timeline()
+        vaqmr.twitter_home_timeline()
     elif collector == 'web_scrape':
-        web_scrape()
+        vaqmr.web_scrape()
     else:
         raise ValueError(f'Unknown collector type: {collector}')
-
-
-def twitter_faves():
-    config = vaqmr.get_config('twitter_faves')
-    secrets = vaqmr.get_secrets()
-
-    for account in config['work_list']:
-        url = 'https://api.twitter.com/1.1/favorites/list.json'
-        params = {'count': 100, 'screen_name': account}
-        headers = {'Authorization': secrets['twitter']['Bearer']}
-
-        faves = requests.get(url, params=params, headers=headers)
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        blob_name = f'{config["output_prefix"]}{account}/{timestamp}.raw'
-
-        vaqmr.upload_data(config['output_bucket'], blob_name, faves.text)
-
-def twitter_timeline():
-    config = vaqmr.get_config('twitter_timeline')
-    secrets = vaqmr.get_secrets()
-
-    for account in config['work_list']:
-        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-        params = {'count': 50, 'screen_name': account, 'include_rts': True}
-        headers = {'Authorization': secrets['twitter']['Bearer']}
-
-        faves = requests.get(url, params=params, headers=headers)
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        blob_name = f'{config["output_prefix"]}{account}/{timestamp}.raw'
-
-        vaqmr.upload_data(config['output_bucket'], blob_name, faves.text)
-
-def twitter_home_timeline():
-    config = vaqmr.get_config('twitter_home_timeline')
-    secrets = vaqmr.get_secrets()
-
-    # looping through work_list doesn't make sense when we only have 1 bearer token
-    for account in config['work_list']:
-        url = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
-        client_key = secrets['twitter']['API key']
-        client_secret = secrets['twitter']['API secret key']
-        resource_owner_key = secrets['twitter']['Access token']
-        resource_owner_secret = secrets['twitter']['Access token secret']
-
-        header_oauth = OAuth1(client_key, client_secret,
-                                                resource_owner_key, resource_owner_secret,
-                                                signature_type='auth_header')
-
-        params = {'count': 100}
-
-        faves = requests.get(url, auth=header_oauth, params=params)
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        blob_name = f'{config["output_prefix"]}{account}/{timestamp}.raw'
-
-        vaqmr.upload_data(config['output_bucket'], blob_name, faves.text)
-
-def web_scrape():
-    config = vaqmr.get_config('web_scrape')
-
-    for site in config['work_list']:
-        storage_key = site['key']
-        url = site['url']
-        headers = {'User-Agent': 'Mozilla/5.0'}
-
-        faves = requests.get(url, headers=headers)
-        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        blob_name = f'{config["output_prefix"]}{storage_key}/{timestamp}.raw'
-
-        vaqmr.upload_data(config['output_bucket'], blob_name, faves.text)
--- vaqmr.py
+++ vaqmr.py
@@ -5,10 +5,19 @@ All data collection for DOTUFP is done through these functions.
 
 import json
 import yaml
+import requests
+from requests_oauthlib import OAuth1
+from datetime import datetime
 from google.cloud import kms_v1
 from google.cloud import storage
 
-def get_config(config_key: str =None):
+def _get_timestamp():
+    return datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+
+def _get_blob_name(output_prefix, account, timestamp):
+    return f'{output_prefix}{account}/{timestamp}.raw'
+
+def _get_config(config_key: str =None):
     """Fetch requested configurations from project config."""
     with open('config.yml', 'r') as config_file:
         project_config = yaml.safe_load(config_file)
@@ -18,8 +27,7 @@ def get_config(config_key: str =None):
     else:
         return project_config
 
-
-def get_secrets():
+def _get_secrets():
     """Fetch and decrypt project secrets."""
     # get encrypted secrets
     storage_client = storage.Client()
@@ -34,11 +42,83 @@ def get_secrets():
 
     return json.loads(secrets.plaintext)
 
-
-def upload_data(bucket_name: str, blob_name: str, data: str):
+def _upload_data(bucket_name: str, blob_name: str, data: str):
     """Upload data to storage."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
     blob.upload_from_string(data)
+
+
+def twitter_faves():
+    """Collect recent faves/likes for twitter accounts defined in project config. Triggered by Pub/Sub."""
+    config = _get_config('twitter_faves')
+    secrets = _get_secrets()
+
+    for account in config['work_list']:
+        url = 'https://api.twitter.com/1.1/favorites/list.json'
+        params = {'count': 100, 'screen_name': account}
+        headers = {'Authorization': secrets['twitter']['Bearer']}
+
+        faves = requests.get(url, params=params, headers=headers)
+
+        timestamp = _get_timestamp()
+        blob_name = _get_blob_name(config["output_prefix"], account, timestamp)
+        _upload_data(config['output_bucket'], blob_name, faves.text)
+
+def twitter_timeline():
+    """Collect recent tweets/retweets for twitter accounts defined in project config. Triggered by Pub/Sub."""
+    config = _get_config('twitter_timeline')
+    secrets = _get_secrets()
+
+    for account in config['work_list']:
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        params = {'count': 50, 'screen_name': account, 'include_rts': True}
+        headers = {'Authorization': secrets['twitter']['Bearer']}
+
+        faves = requests.get(url, params=params, headers=headers)
+
+        timestamp = _get_timestamp()
+        blob_name = _get_blob_name(config["output_prefix"], account, timestamp)
+        _upload_data(config['output_bucket'], blob_name, faves.text)
+
+def twitter_home_timeline():
+    """Collect recent tweets seen by twitter accounts defined in project config. Triggered by Pub/Sub."""
+    config = _get_config('twitter_home_timeline')
+    secrets = _get_secrets()
+
+    # looping through work_list doesn't make sense when we only have 1 bearer token
+    for account in config['work_list']:
+        url = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
+        client_key = secrets['twitter']['API key']
+        client_secret = secrets['twitter']['API secret key']
+        resource_owner_key = secrets['twitter']['Access token']
+        resource_owner_secret = secrets['twitter']['Access token secret']
+
+        header_oauth = OAuth1(client_key, client_secret,
+                                                resource_owner_key, resource_owner_secret,
+                                                signature_type='auth_header')
+
+        params = {'count': 100}
+
+        faves = requests.get(url, auth=header_oauth, params=params)
+
+        timestamp = _get_timestamp()
+        blob_name = _get_blob_name(config["output_prefix"], account, timestamp)
+        _upload_data(config['output_bucket'], blob_name, faves.text)
+
+def web_scrape():
+    """Capture public websites defined in project config. Triggered by Pub/Sub."""
+    config = _get_config('web_scrape')
+
+    for site in config['work_list']:
+        storage_key = site['key']
+        url = site['url']
+        headers = {'User-Agent': 'Mozilla/5.0'}
+
+        faves = requests.get(url, headers=headers)
+
+        timestamp = _get_timestamp()
+        blob_name = _get_blob_name(config["output_prefix"], storage_key, timestamp)
+        _upload_data(config['output_bucket'], blob_name, faves.text)
